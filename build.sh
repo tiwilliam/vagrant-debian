592cdf034cabc4e4f5e89fee803d47a0bc1ddbd1
William Tisäter <william@defunct.cc>
2012-06-22 12:27:39 +0200

    Retab to spaces

--- build.sh
+++ build.sh
@@ -35,12 +35,12 @@ DEBIAN_ISO_URL="${DEBIAN_URL}/${DEBIAN_ISO_NAME}"
 DEBIAN_ISO_FILE="${FOLDER_ISO}/${DEBIAN_ISO_NAME}"
 
 function abort {
-	echo >&2 "ERROR: $1"
-	exit 1
+    echo >&2 "ERROR: $1"
+    exit 1
 }
 
 function info {
-	echo "INFO: $1"
+    echo "INFO: $1"
 }
 
 # Check if VM name is occupied
@@ -67,7 +67,7 @@ mkdir -p "${FOLDER_ISO}/initrd"
 # Download ISO if needed
 info "Downloading ${DEBIAN_ISO_NAME}..."
 if [ ! -f "${DEBIAN_ISO_FILE}" ]; then
-	curl --progress-bar -o "${DEBIAN_ISO_FILE}" -L "${DEBIAN_ISO_URL}"
+    curl --progress-bar -o "${DEBIAN_ISO_FILE}" -L "${DEBIAN_ISO_URL}"
 fi
 
 # Command to get MD5 hash from server
@@ -75,12 +75,12 @@ ISO_MD5=$(curl -s "${DEBIAN_URL}/MD5SUMS" | grep "${DEBIAN_ISO_NAME}" | awk '{ p
 
 # Check if hash is correct
 if [ ! "${ISO_MD5}" ]; then
-	info "Faild to download MD5 hash for ${DEBIAN_ISO_NAME}. Skipping."
+    info "Faild to download MD5 hash for ${DEBIAN_ISO_NAME}. Skipping."
 else
-	ISO_HASH=$(md5 -q "${DEBIAN_ISO_FILE}")
-	if [ "${ISO_MD5}" != "${ISO_HASH}" ]; then
-		abort "MD5 does not match - expected ${ISO_MD5}. Aborting."
-	fi
+    ISO_HASH=$(md5 -q "${DEBIAN_ISO_FILE}")
+    if [ "${ISO_MD5}" != "${ISO_HASH}" ]; then
+        abort "MD5 does not match - expected ${ISO_MD5}. Aborting."
+    fi
 fi
 
 info "Unpacking ${DEBIAN_ISO_NAME}..."
@@ -90,9 +90,9 @@ info "Customizing ISO files..."
 chmod -R u+w "${FOLDER_ISO}/custom"
 
 pushd "${FOLDER_ISO}/initrd"
-	gunzip -c "${FOLDER_ISO}/custom/${FOLDER_INSTALL}/initrd.gz" | cpio -id
-	cp "${FOLDER_BASE}/conf/preseed.cfg" "${FOLDER_ISO}/initrd/preseed.cfg"
-	find . | cpio --create --format='newc' | gzip  > "${FOLDER_ISO}/custom/${FOLDER_INSTALL}/initrd.gz"
+    gunzip -c "${FOLDER_ISO}/custom/${FOLDER_INSTALL}/initrd.gz" | cpio -id
+    cp "${FOLDER_BASE}/conf/preseed.cfg" "${FOLDER_ISO}/initrd/preseed.cfg"
+    find . | cpio --create --format='newc' | gzip  > "${FOLDER_ISO}/custom/${FOLDER_INSTALL}/initrd.gz"
 popd
 
 cp "${FOLDER_BASE}/conf/late_command.sh" "${FOLDER_ISO}/custom/late_command.sh"
@@ -100,66 +100,66 @@ cp "${FOLDER_BASE}/conf/isolinux.${ARCH}.cfg" "${FOLDER_ISO}/custom/isolinux/iso
 
 info "Packing ISO files..."
 mkisofs -r -V "Custom Debian Install CD" -cache-inodes -quiet -J -l \
-	-b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
-	-boot-load-size 4 -boot-info-table -o "${FOLDER_ISO}/custom.iso" \
-	"${FOLDER_ISO}/custom"
+    -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
+    -boot-load-size 4 -boot-info-table -o "${FOLDER_ISO}/custom.iso" \
+    "${FOLDER_ISO}/custom"
 
 chmod -R u-w "${FOLDER_ISO}/custom"
 
 info "Creating VM..."
 VBoxManage createvm \
-	--name "${BOX}" \
-	--ostype Debian \
-	--register \
-	--basefolder "${FOLDER_VBOX}"
-	
+    --name "${BOX}" \
+    --ostype Debian \
+    --register \
+    --basefolder "${FOLDER_VBOX}"
+    
 VBoxManage modifyvm "${BOX}" \
-	--memory 360 \
-	--boot1 dvd \
-	--boot2 disk \
-	--boot3 none \
-	--boot4 none \
-	--vram 12 \
-	--pae off \
-	--rtcuseutc on
-	
+    --memory 360 \
+    --boot1 dvd \
+    --boot2 disk \
+    --boot3 none \
+    --boot4 none \
+    --vram 12 \
+    --pae off \
+    --rtcuseutc on
+    
 VBoxManage storagectl "${BOX}" \
-	--name "IDE Controller" \
-	--add ide \
-	--controller PIIX4 \
-	--hostiocache on
-	
+    --name "IDE Controller" \
+    --add ide \
+    --controller PIIX4 \
+    --hostiocache on
+    
 VBoxManage storageattach "${BOX}" \
-	--storagectl "IDE Controller" \
-	--port 1 \
-	--device 0 \
-	--type dvddrive \
-	--medium "${FOLDER_ISO}/custom.iso"
-	
+    --storagectl "IDE Controller" \
+    --port 1 \
+    --device 0 \
+    --type dvddrive \
+    --medium "${FOLDER_ISO}/custom.iso"
+    
 VBoxManage storagectl "${BOX}" \
-	--name "SATA Controller" \
-	--add sata \
-	--controller IntelAhci \
-	--sataportcount 1 \
-	--hostiocache off
-	
+    --name "SATA Controller" \
+    --add sata \
+    --controller IntelAhci \
+    --sataportcount 1 \
+    --hostiocache off
+    
 VBoxManage createhd \
-	--filename "${FOLDER_VBOX}/${BOX}/${BOX}.vdi" \
-	--size 40960
-	
+    --filename "${FOLDER_VBOX}/${BOX}/${BOX}.vdi" \
+    --size 40960
+    
 VBoxManage storageattach "${BOX}" \
-	--storagectl "SATA Controller" \
-	--port 0 \
-	--device 0 \
-	--type hdd \
-	--medium "${FOLDER_VBOX}/${BOX}/${BOX}.vdi"
+    --storagectl "SATA Controller" \
+    --port 0 \
+    --device 0 \
+    --type hdd \
+    --medium "${FOLDER_VBOX}/${BOX}/${BOX}.vdi"
 
 info "Booting VM..."
 VBoxManage startvm "${BOX}"
 
 info "Waiting for installer..."
 while VBoxManage list runningvms | grep "${BOX}" > /dev/null; do
-	sleep 10
+    sleep 10
 done
 
 info "Building Vagrant box..."
