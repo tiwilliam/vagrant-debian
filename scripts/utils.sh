75bbb5ae34c80b71cc9d86f1e27a834397a09a75
William Tisäter <william@defunct.cc>
2014-05-03 02:17:13 +0200

    Add some color logging

---
+++ scripts/utils.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+txtblk=$'\e[0;30m' # Black - Regular
+txtred=$'\e[0;31m' # Red
+txtgrn=$'\e[0;32m' # Green
+txtylw=$'\e[0;33m' # Yellow
+txtblu=$'\e[0;34m' # Blue
+txtpur=$'\e[0;35m' # Purple
+txtcyn=$'\e[0;36m' # Cyan
+txtwht=$'\e[0;37m' # White
+txtrst=$'\e[0m'    # Text Reset
+
+function abort {
+    echo -e >&2 "${txtred}ERROR: $1${txtrst}"
+    exit 1
+}
+
+function info {
+    echo -e "${txtpur}INFO: $1${txtrst}"
+}
+
+function warn {
+    echo -e "${txtylw}WARN: $1${txtrst}"
+}
